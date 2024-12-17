74882348bc399644786605e2885eb55f033cce10
Vivienne126 <vivienne.singh@cns.ac.in>
2024-12-17 17:33:57 +0530

    New1

---
+++ Activity2.py
@@ -0,0 +1,12 @@
+name="Penguin"
+Age= 4
+Weight= 37.5
+print("The datatype is ", type(name))
+print("The datatype is ", type(Age))
+print("The datatype is ", type(Weight))
+Age=str(Age)
+Weight=int(Weight)
+print(Age)
+print(Weight)
+print(type(Age))
+print(type(Weight))
