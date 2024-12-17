43ac1022064d16463335ea906bf689c3829fc616
Vivienne126 <vivienne.singh@cns.ac.in>
2024-12-17 17:21:02 +0530

    new

---
+++ Activithy1.py
@@ -0,0 +1,8 @@
+a= 5
+b= 1.5
+c= True
+d="Hello"
+print("The datatype of a is" , type(a))
+print("The datatype of b is" , type(b))
+print("The datatype of c is" , type(c))
+print("The datatype of d is" , type(d))
